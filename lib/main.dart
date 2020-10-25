61cbcfb4f9b4cd0408f10282eb15a744c96e303f
Juan Manuel <juamaluna2004@gmail.com>
2020-10-25 17:36:09 -0500

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:seven_hub/src/Splash.dart';
 
-
 void main() => runApp(MaterialApp(
   home: Splash(),
-  title: 'Nombre de la Appp',
+  title: 'Nombre de la App',
   debugShowCheckedModeBanner: false,
 
 ));
