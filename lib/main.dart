45fac03f37b38378707ea7307cd7139a76628f8d
Juan Manuel <juamaluna2004@gmail.com>
2020-10-25 17:31:20 -0500

    Podfile Actualización

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:seven_hub/src/LoginScreen.dart';
 import 'package:seven_hub/src/Splash.dart';
+
 
 void main() => runApp(MaterialApp(
   home: Splash(),
-  title: 'Nombre de la App',
+  title: 'Nombre de la Appp',
   debugShowCheckedModeBanner: false,
 
 ));
