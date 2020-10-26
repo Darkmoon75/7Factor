dee37ceb738dfc0ee47cc7cad560fcee1ae90b19
Juan Manuel <juamaluna2004@gmail.com>
2020-10-25 20:11:58 -0500

    Autenticación de Google completada

--- lib/src/LoginScreen.dart
+++ lib/src/LoginScreen.dart
@@ -4,17 +4,26 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final Color primaryColor = Color(0xff18203d);
+
   final Color secondaryColor = Color(0xff232c51);
 
   final Color logoGreen = Color(0xff25bcbb);
 
-  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-  final GoogleSignIn googleSignIn = GoogleSignIn();
-
   final TextEditingController nameController = TextEditingController();
+
   final TextEditingController passwordController = TextEditingController();
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  User _user;
+
+  GoogleSignIn _googleSignIn = new GoogleSignIn();
 
   @override
   Widget build(BuildContext context) {
@@ -24,72 +33,124 @@ class LoginScreen extends StatelessWidget {
           elevation: 0,
         ),
         backgroundColor: primaryColor,
-        body: Container(
-          alignment: Alignment.topCenter,
-          margin: EdgeInsets.symmetric(horizontal: 30),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Sign in to 7Factor and continue',
-                  textAlign: TextAlign.center,
-                  style:
-                      GoogleFonts.openSans(color: Colors.white, fontSize: 28),
+        body: isSignIn
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(_user.photoUrl),
+                    ),
+                    Text(_user.displayName),
+                    OutlineButton(
+                      onPressed: () {
+                        gooleSignout();
+                      },
+                      child: Text("Logout"),
+                    )
+                  ],
                 ),
-                SizedBox(height: 20),
-                Text(
-                  'Enter your email and password below to continue to 7Factor and let the learning begin!',
-                  textAlign: TextAlign.center,
-                  style:
-                      GoogleFonts.openSans(color: Colors.white, fontSize: 14),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                _buildTextField(
-                    nameController, Icons.account_circle, 'Username'),
-                SizedBox(height: 20),
-                _buildTextField(passwordController, Icons.lock, 'Password'),
-                SizedBox(height: 30),
-                MaterialButton(
-                  elevation: 0,
-                  minWidth: double.maxFinite,
-                  height: 50,
-                  onPressed: () {},
-                  color: logoGreen,
-                  child: Text('Login',
-                      style: TextStyle(color: Colors.white, fontSize: 16)),
-                  textColor: Colors.white,
-                ),
-                SizedBox(height: 20),
-                MaterialButton(
-                  elevation: 0,
-                  minWidth: double.maxFinite,
-                  height: 50,
-                  onPressed: _signInWithGoogle,
-                  color: Colors.blue,
-                  child: Row(
+              )
+            : Container(
+                alignment: Alignment.topCenter,
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                child: SingleChildScrollView(
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
-                      Icon(FontAwesomeIcons.google),
-                      SizedBox(width: 10),
-                      Text('Sign-in using Google',
-                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                      Text(
+                        'Sign in to 7Factor and continue',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.openSans(
+                            color: Colors.white, fontSize: 28),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Enter your email and password below to continue to 7Factor and let the learning begin!',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.openSans(
+                            color: Colors.white, fontSize: 14),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      _buildTextField(
+                          nameController, Icons.account_circle, 'Username'),
+                      SizedBox(height: 20),
+                      _buildTextField(
+                          passwordController, Icons.lock, 'Password'),
+                      SizedBox(height: 30),
+                      MaterialButton(
+                        elevation: 0,
+                        minWidth: double.maxFinite,
+                        height: 50,
+                        onPressed: () {},
+                        color: logoGreen,
+                        child: Text('Login',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 16)),
+                        textColor: Colors.white,
+                      ),
+                      SizedBox(height: 20),
+                      MaterialButton(
+                        elevation: 0,
+                        minWidth: double.maxFinite,
+                        height: 50,
+                        onPressed: () {
+                          handleSignIn();
+                        },
+                        color: Colors.blue,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(FontAwesomeIcons.google),
+                            SizedBox(width: 10),
+                            Text('Sign-in using Google',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 16)),
+                          ],
+                        ),
+                        textColor: Colors.white,
+                      ),
+                      SizedBox(height: 100),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: _buildFooterLogo(),
+                      )
                     ],
                   ),
-                  textColor: Colors.white,
                 ),
-                SizedBox(height: 100),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: _buildFooterLogo(),
-                )
-              ],
-            ),
-          ),
-        ));
+              ));
+  }
+
+  bool isSignIn = false;
+
+  Future<void> handleSignIn() async {
+    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    UserCredential result = (await _auth.signInWithCredential(credential));
+
+    _user = result.user;
+
+    setState(() {
+      isSignIn = true;
+    });
+  }
+
+  Future<void> gooleSignout() async {
+    await _auth.signOut().then((onValue) {
+      _googleSignIn.signOut();
+      setState(() {
+        isSignIn = false;
+      });
+    });
   }
 
   _buildFooterLogo() {
@@ -131,19 +192,5 @@ class LoginScreen extends StatelessWidget {
             border: InputBorder.none),
       ),
     );
-  }
-
-  _signInWithGoogle() async {
-    final GoogleSignInAccount googleUser = await googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-
-    // ignore: deprecated_member_use
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-        idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
-
-    // ignore: deprecated_member_use
-    final FirebaseUser user =
-        (await firebaseAuth.signInWithCredential(credential)).user;
   }
 }
