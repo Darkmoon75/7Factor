b481fb2480fee8d3c2e8ac206483e23d9482fb8d
Andres Diaz <andresf.diazm@unac.edu.co>
2020-10-26 19:13:55 -0500

    Bottom Navigator Bar

--- lib/src/HomeScreen.dart
+++ lib/src/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -8,6 +9,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +19,32 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.blue,
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Bienvenido, Dashboard'),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.class_),
+              title: Text('Mis Clases'),
+              backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              title: Text('Foro'),
+              backgroundColor: Colors.green),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_pin),
+              title: Text('Tutores'),
+              backgroundColor: Colors.blueGrey),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Mi Perfil'),
+              backgroundColor: Colors.blueAccent),
         ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
