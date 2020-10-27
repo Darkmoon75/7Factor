c6a71a90cf93a042563a71a79a12756c59e9cb1c
Juan Manuel <juamaluna2004@gmail.com>
2020-10-27 11:41:06 -0500

    Ultimos cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:seven_hub/src/HomeScreen.dart';
 import 'package:seven_hub/src/Splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -7,7 +6,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(
-    home: HomeScreen(),
+    home: Splash(),
     title: 'Nombre de la App',
     debugShowCheckedModeBanner: false,
   ));
--- lib/src/HomeScreen.dart
+++ lib/src/HomeScreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'tutores.dart';
 import 'clases.dart';
 import 'foro.dart';
