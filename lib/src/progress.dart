755dd667413cc3c27c888a71850f51f6076c066a
Andres Diaz <andresf.diazm@unac.edu.co>
2020-10-29 19:55:14 -0500

    Progress Map

--- lib/src/progress.dart
+++ lib/src/progress.dart
@@ -22,15 +22,41 @@ class _ProgressState extends State<Progress> {
           child: Column(
         children: <Widget>[
           Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Container(
-                width: 200,
-                height: 200,
-                child: FlareActor(
-                  "images/Chest.flr",
-                  animation: "Play",
-                ),
-              ))
+            padding: const EdgeInsets.all(32.0),
+            child: Container(
+              width: 200,
+              height: 200,
+              child: FlareActor(
+                "images/Chest.flr",
+                animation: "Play",
+              ),
+            ),
+          ),
+          Text(
+            "¡Sigue estudiando para llegar a tu recompensa!",
+            style: TextStyle(
+              fontSize: 15,
+              foreground: Paint()
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = 1
+                ..color = Colors.blue[700],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(23.0),
+            child: Container(
+              width: 300,
+              height: 300,
+              child: FlareActor(
+                "images/map.flr",
+                animation: "Move_1",
+              ),
+            ),
+          ),
+          Text(
+            "Esfuérzate y anímate, no temas, ni tengas miedo de ellos, porque el Señor va contigo, no te dejará, ni te desamparará. Deuteronomio 31:6",
+            style: TextStyle(color: Colors.black),
+          ),
         ],
       )),
     );
